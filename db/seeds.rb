95a1cf8fe3442b046a347e6e92c4ff075b38e63f
Bruno Silva <info.bsilva@gmail.com>
2021-03-19 15:37:30 +0100

    Rails - Day 4 - Exo 2 - Gemfile modifed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,46 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+
+puts "*****************************************************************"
+
+puts "Cleaning database..."
+
+Ingredient.destroy_all
+
+puts "Parsing JSON..."
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+ingredient_serialized = open(url).read
+
+ingredient = JSON.parse(ingredient_serialized)
+
+puts "Creating ingredients list..."
+
+ingredient["drinks"].each do |element|
+ingredient_name = Ingredient.new(name: element["strIngredient1"])
+
+puts "Saving ingredients #{ingredient_name.name}..."
+ingredient_name.save
+
+end
+
+puts "Finished!"
+
+puts "*****************************************************************"
+
+puts "Cleaning database..."
+
+Cocktail.destroy_all
+
+puts "Creating Cocktails..."
+
+Cocktail.create(name: "Manhattan")
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Dry Martini")
+
+puts "Finished!"
+
+puts "*****************************************************************"
 
