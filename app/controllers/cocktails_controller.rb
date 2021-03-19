7b26993f22d9059e3eed6bb1abdaf5adea29b857
Bruno Silva <info.bsilva@gmail.com>
2021-03-19 11:16:50 +0100

    Rails - Day 3 - Exo 2 - HTML update, some back correction - OK

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,31 @@
 class CocktailsController < ApplicationController
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def new
+    @cocktail = Cocktail.new#(params[:cocktail])
+  end
+
+  def create
+    @cocktail = Cocktail.create(params_cocktail)
+      if @cocktail.save
+        redirect_to cocktail_path(@cocktail)
+      else
+        render :new
+      end
+  end
+    
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+  end   
+
+  private
+  
+  def params_cocktail
+    params.require(:cocktail).permit(:name)
+  end
+
 end
